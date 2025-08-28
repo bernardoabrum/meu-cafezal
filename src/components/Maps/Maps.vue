ede0c16e8bf9f581b95dad1e21d231d00fd4f844
Bernardo <bernardoalvesbrum_100@hotmail.com>
2025-08-28 09:22:08 -0300

    fix: switch maps plugin

--- src/components/Maps/Maps.vue
+++ src/components/Maps/Maps.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="map-container">
-    <GMapMap
+    <GoogleMap
+      :api-key="VITE_GOOGLE_MAPS_API_KEY"
       :center="center"
       :zoom="15"
-      style="width: 100%; height: 100%"
+      style="width: 1000px; height: 500px"
       map-type-id="hybrid"
       :options="{
         mapTypeControl: false,
@@ -17,22 +18,20 @@
 <script setup>
 import "./Maps.scss";
 import { ref, onMounted } from "vue";
+import { GoogleMap } from "vue3-google-map";
 
-const center = ref({ lat: -23.55052, lng: -46.633308 });
+const { VITE_GOOGLE_MAPS_API_KEY } = import.meta.env;
+
+const center = ref({ lat: -23.55052, lng: -46.633308 }); // localização padrão (São Paulo)
 
 onMounted(() => {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (pos) => {
-        center.value = {
-          lat: pos.coords.latitude,
-          lng: pos.coords.longitude,
-        };
-      },
-      (err) => {
-        console.error("Erro ao obter localização:", err);
-      }
-    );
+    navigator.geolocation.getCurrentPosition((pos) => {
+      center.value = {
+        lat: pos.coords.latitude,
+        lng: pos.coords.longitude,
+      };
+    });
   }
 });
 </script>
