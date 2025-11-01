07428809a19680f9de4a093ba3c9d216b4742f48
Bernardo <bernardoalvesbrum_100@hotmail.com>
2025-11-01 19:59:48 -0300

    feat: no properties rule

--- src/pages/Statistics/Statistics.vue
+++ src/pages/Statistics/Statistics.vue
@@ -1,76 +1,81 @@
 <template>
   <div class="page statistics-page">
     <BackButton />
-    <div class="select">
-      <select v-model="selectedYear" name="selectedYear">
-        <option disabled value="">Selecione um ano</option>
-        <option v-for="year in registeredYears" :key="year" :value="year">
-          {{ year }}
-        </option>
-      </select>
+    <div v-if="properties.length">
+      <div class="select">
+        <select v-model="selectedYear" name="selectedYear">
+          <option disabled value="">Selecione um ano</option>
+          <option v-for="year in registeredYears" :key="year" :value="year">
+            {{ year }}
+          </option>
+        </select>
+      </div>
+      <div class="infos">
+        <div class="totals">
+          <h2 class="title">Estatísticas gerais ({{ selectedYear }})</h2>
+          <p>
+            Produção total em volumes:
+            <span>{{ totalProduction[selectedYear] || 0 }}</span>
+          </p>
+          <p>
+            Produção total em sacas (estimado):
+            <span>{{
+              ((totalProduction[selectedYear] ?? 0) / 8.33).toFixed(1)
+            }}</span>
+          </p>
+        </div>
+        <div>
+          <PieChart
+            :selectedYear="selectedYear"
+            :property="properties"
+            type="properties"
+          />
+        </div>
+        <div class="line-chart" v-if="registeredYears.length > 1">
+          <h2 class="title">Evolução da produção ao longo dos anos</h2>
+          <LineChart :chartData="totalProduction" />
+        </div>
+      </div>
+      <div class="table">
+        <h2 class="title">
+          Estatísticas de cada propriedade na proporção por hectare ({{
+            selectedYear
+          }})
+        </h2>
+        <table v-if="properties.length">
+          <thead>
+            <tr>
+              <th>Propriedade</th>
+              <th>Volumes por hectare</th>
+              <th>Sacas por hectare (estimado)</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="property in productionPerHectare" :key="property.name">
+              <td>{{ property.name }}</td>
+              <td>{{ property.volumesPerHectare.toFixed(1) }}</td>
+              <td>{{ property.bagsPerHectare.toFixed(1) }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="graphs">
+        <h2 class="title">
+          Distribuição da produção por talhão ({{ selectedYear }})
+        </h2>
+        <div class="pie-chart">
+          <PieChart
+            v-for="property in properties"
+            :key="property.id"
+            :selectedYear="selectedYear"
+            :property="property"
+            type="fields"
+          />
+        </div>
+      </div>
     </div>
-    <div class="infos">
-      <div class="totals">
-        <h2 class="title">Estatísticas gerais ({{ selectedYear }})</h2>
-        <p>
-          Produção total em volumes:
-          <span>{{ totalProduction[selectedYear] || 0 }}</span>
-        </p>
-        <p>
-          Produção total em sacas (estimado):
-          <span>{{
-            ((totalProduction[selectedYear] ?? 0) / 8.33).toFixed(1)
-          }}</span>
-        </p>
-      </div>
-      <div>
-        <PieChart
-          :selectedYear="selectedYear"
-          :property="properties"
-          type="properties"
-        />
-      </div>
-      <div class="line-chart" v-if="registeredYears.length > 1">
-        <h2 class="title">Evolução da produção ao longo dos anos</h2>
-        <LineChart :chartData="totalProduction" />
-      </div>
-    </div>
-    <div class="table">
-      <h2 class="title">
-        Estatísticas de cada propriedade na proporção por hectare ({{
-          selectedYear
-        }})
-      </h2>
-      <table v-if="properties.length">
-        <thead>
-          <tr>
-            <th>Propriedade</th>
-            <th>Volumes por hectare</th>
-            <th>Sacas por hectare (estimado)</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="property in productionPerHectare" :key="property.name">
-            <td>{{ property.name }}</td>
-            <td>{{ property.volumesPerHectare.toFixed(1) }}</td>
-            <td>{{ property.bagsPerHectare.toFixed(1) }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    <div class="graphs">
-      <h2 class="title">
-        Distribuição da produção por talhão ({{ selectedYear }})
-      </h2>
-      <div class="pie-chart">
-        <PieChart
-          v-for="property in properties"
-          :key="property.id"
-          :selectedYear="selectedYear"
-          :property="property"
-          type="fields"
-        />
-      </div>
+    <div v-else class="no-properties">
+      <h2>Você precisa marcar propriedades para ter acesso as estatísticas!</h2>
     </div>
   </div>
 </template>
