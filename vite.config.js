1d365c1a9f79c230f162056f3662af6f49604309
Bernardo <bernardoalvesbrum_100@hotmail.com>
2025-11-16 22:34:14 -0300

    fix : removing vue devtools

--- vite.config.js
+++ vite.config.js
@@ -2,53 +2,60 @@ import { fileURLToPath, URL } from "node:url";
 import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vueDevTools from "vite-plugin-vue-devtools";
 
-export default defineConfig({
-  define: {
-    __VUE_PROD_DEVTOOLS__: false,
-  },
-  plugins: [
-    vue(),
-    process.env.NODE_ENV === "development" && vueDevTools(),
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "Meu Cafezal",
-        short_name: "Meu Cafezal",
-        theme_color: "#14171f",
-        icons: [
-          {
-            src: "/icons/pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/icons/pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-        screenshots: [
-          {
-            src: "/screenshots/screenshot1.png",
-            sizes: "952x532",
-            type: "image/png",
-            form_factor: "wide",
-          },
-          {
-            src: "/screenshots/screenshot2.png",
-            sizes: "695x821",
-            type: "image/png",
-            form_factor: "narrow",
-          },
-        ],
-      },
-    }),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(async ({ mode }) => {
+  const isDev = mode === "development";
+
+  const vueDevTools = isDev
+    ? (await import("vite-plugin-vue-devtools")).default()
+    : null;
+
+  return {
+    define: {
+      __VUE_PROD_DEVTOOLS__: false,
     },
-  },
+    plugins: [
+      vue(),
+      vueDevTools,
+      VitePWA({
+        registerType: "autoUpdate",
+        manifest: {
+          name: "Meu Cafezal",
+          short_name: "Meu Cafezal",
+          theme_color: "#14171f",
+          icons: [
+            {
+              src: "/icons/pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "/icons/pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
+          screenshots: [
+            {
+              src: "/screenshots/screenshot1.png",
+              sizes: "952x532",
+              type: "image/png",
+              form_factor: "wide",
+            },
+            {
+              src: "/screenshots/screenshot2.png",
+              sizes: "695x821",
+              type: "image/png",
+              form_factor: "narrow",
+            },
+          ],
+        },
+      }),
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  };
 });
